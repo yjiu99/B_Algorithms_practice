0dbf617397879dca6b3d100da91fd3125a179082
yjiu99 <yjiu9992@gmail.com>
2023-06-08 21:15:21 +0900

    B11382

---
+++ src/B11382.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 브론즈 5 꼬마 정민
+public class B11382 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(a+b+c);
+
+    }
+}
