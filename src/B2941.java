859121887d764f95fa1096e1af05582c32b0fa6a
yjiu99 <yjiu9992@gmail.com>
2023-07-09 23:50:24 +0900

    B2941

---
+++ src/B2941.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int len = str.length();
+        int cnt = 0;
+
+        for(int i =0 ; i <len;i++){
+            char ch = str.charAt(i); // 하나씩 검사
+
+            if(ch == 'c' && i < len-1){ // ch 가 c 이면
+                // ch 다음 문자가 '=' 또는 '-'이면
+                if(str.charAt(i+1) == '=' || str.charAt(i+1)=='-'){
+                    i++;
+                }
+
+            } else if (ch == 'd' && i < len-1) {
+                if(str.charAt(i+1) == '-')
+                    i++;
+                else if (str.charAt(i+1) == 'z' && i< len -2) {
+                    if(str.charAt(i+2) == '=')
+                        i += 2;
+                }
+            } else if ((ch == 'l' || ch == 'n') && i < len -1) {
+                if(str.charAt(i+1) == 'j')
+                    i++;
+            } else if ((ch == 's' || ch == 'z') && i < len -1) {
+                if(str.charAt(i+1) == '=')
+                    i++;
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
